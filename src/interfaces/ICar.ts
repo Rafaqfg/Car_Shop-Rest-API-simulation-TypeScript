1edfb0acf0fdbaba8ae215e4638298d1f634315a
Rafaqfg <rafa.qfg@gmail.com>
2022-09-15 06:03:14 -0300

    feat: :sparkles: created CarSchema extending from VehicleSchema and created ICar type

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+import VehicleSchema from './IVehicle';
+
+const CarSchema = VehicleSchema.extend({
+  doorsQty: z.number().int().positive().gte(2)
+    .lte(4),
+  seatsQty: z.number().int().positive().gte(2)
+    .lte(7),
+});
+
+type ICar = z.infer<typeof CarSchema>;
+
+export default CarSchema;
+export { ICar };
