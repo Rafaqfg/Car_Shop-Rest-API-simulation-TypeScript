71f5e36f258b56518b899a3d505dedf7d8e6e92b
Rafaqfg <rafa.qfg@gmail.com>
2022-09-16 07:26:32 -0300

    feat: :sparkles: updated app to use CarsRoute and read json file

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import 'express-async-errors';
+import CarsRoute from './routes/carsRoute';
 
 const app = express();
+app.use(express.json());
+
+app.use(CarsRoute);
 
 export default app;
