13a0e4c8c51601015e4702b972f99321bcb9b80e
Rafaqfg <rafa.qfg@gmail.com>
2022-09-16 05:30:36 -0300

    feat: :sparkles: created generic class MongoModel and the respective methods

---
+++ src/models/MongoModel.ts
@@ -0,0 +1,40 @@
+import { Model, isValidObjectId, UpdateQuery } from 'mongoose';
+import { IModel } from '../interfaces/IModel';
+
+abstract class MongoModel<T> implements IModel<T> {
+  protected _model:Model<T>;
+
+  constructor(private model:Model<T>) {
+    this._model = model;
+  }
+
+  public async create(obj: T): Promise<T> {
+    return this._model.create({ ...obj });
+  }
+
+  public async read(): Promise<T[]> {
+    return this._model.find();
+  }
+
+  public async readOne(_id: string): Promise<T | null> {
+    if (!isValidObjectId(_id)) throw Error('invalidMongoId');
+    return this._model.findOne({ _id });
+  }
+
+  public async update(_id: string, payload: Partial<T>): Promise<T | null> {
+    if (!isValidObjectId(_id)) throw Error('invalidMongoId');
+    const result = await this._model.findByIdAndUpdate(
+      { _id },
+      { ...payload } as UpdateQuery<T>,
+      { new: true },
+    );
+    return result as T;
+  }
+
+  public async delete(_id: string): Promise<T | null> {
+    if (!isValidObjectId(_id)) throw Error('invalidMongoId');
+    return this._model.findByIdAndDelete(_id);
+  }
+}
+
+export default MongoModel;
