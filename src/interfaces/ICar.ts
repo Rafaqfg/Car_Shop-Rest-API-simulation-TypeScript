89ef8c929cfbe57e203af2e9e43ba13db24c9edc
Rafaqfg <rafa.qfg@gmail.com>
2022-09-18 09:08:20 -0300

    fix: :adhesive_bandage: fixed format of response

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod';
 import VehicleSchema from './IVehicle';
 
-const CarSchema = VehicleSchema.extend({
+const CarSchema = VehicleSchema.merge(z.object({
   doorsQty: z.number().int().positive().gte(2)
     .lte(4),
   seatsQty: z.number().int().positive().gte(2)
     .lte(7),
-});
+}));
 
 type ICar = z.infer<typeof CarSchema>;
 
--- src/models/CarsModel.ts
+++ src/models/CarsModel.ts
@@ -3,9 +3,14 @@ import { ICar } from '../interfaces/ICar';
 import MongoModel from './MongoModel';
 
 const carsMongooseSchema = new Schema<ICar>({
+  model: String,
+  year: Number,
+  color: String,
+  buyValue: Number,
   doorsQty: Number,
   seatsQty: Number,
-});
+  status: Boolean,
+}, { versionKey: false });
 
 class CarsModel extends MongoModel<ICar> {
   constructor(model = mongooseCreateModel('cars', carsMongooseSchema)) {
