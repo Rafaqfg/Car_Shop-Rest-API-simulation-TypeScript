f28fd682d5a34d60f64b71fc8a574f40d250f9d7
Rafaqfg <rafa.qfg@gmail.com>
2022-09-18 11:19:55 -0300

    feat: :sparkles: implemented motorcycleRoute in app file

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import 'express-async-errors';
 import errorHandler from './middleware/errorHandler';
 import CarsRoute from './routes/carsRoute';
+import MotorcycleRoute from './routes/motorcyclesRoute';
 
 const app = express();
 app.use(express.json());
 
 app.use(CarsRoute);
+
+app.use('/motorcycles', MotorcycleRoute);
 
 app.use(errorHandler);
 
